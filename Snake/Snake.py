628fe04ecc34bad0156e678b5d9ee83b61b666a0
Berkay_Dedeoglu <berkay.dedeoglu@yandex.com>
2017-04-24 07:30:19 +0300

    Elma nesnesi eklendi. Carpisma kontrolu eklendi.
    Signed-off-by: Berkay_Dedeoglu <berkay.dedeoglu@yandex.com>

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -1,11 +1,12 @@
+import Apple
 
-class Snake(object):
+class CSnake(object):
     __speed = 3
     __direction = (0, 0)
     __tail_coords = [[1, 0], [0, 0]]
 
     def __init__(self):
-        pass
+        self.new_apple()
 
     @property
     def get_speed(self):
@@ -25,6 +26,10 @@ class Snake(object):
     def get_coordinates(self):
         return self.__tail_coords
 
+    @property
+    def get_apple(self):
+        return self.apple
+
     def move_snake(self):
         # [[2,3], [2,4], [2,5]]
         x = self.__direction[0]
@@ -36,6 +41,13 @@ class Snake(object):
 
         self.__tail_coords[0][0] = x + self.__tail_coords[0][0]
         self.__tail_coords[0][1] = y + self.__tail_coords[0][1]
+
+        if self.check_crash():
+            return -1
+
+        if self.find_apple():
+            self.eat()
+            self.new_apple()
 
         foo = 1
         while foo < length:
@@ -57,3 +69,23 @@ class Snake(object):
             t.append([(t[-1][0]), (t[-1][1]-1)])
         elif self.get_direction == (0, -1):
             t.append([(t[-1][0]), (t[-1][1] + 1)])
+
+    def find_apple(self):
+        coords = self.get_apple.coords
+
+        if self.__tail_coords[0] == list(coords):
+            return True
+        else:
+            return False
+
+    def new_apple(self):
+        self.apple = Apple.Apple()
+
+    def check_crash(self):
+        head = self.__tail_coords[0]
+        li = self.__tail_coords[1:]
+
+        if head in li:
+            return True
+        else:
+            return False
